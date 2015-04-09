2b8a81614dc173ab0dfc5f5f5f12db700a6ac44f
Henry Ng <henryfc.ng@gmail.com>
2015-04-08 22:07:35 -0400

    All Basic Requirements Done

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,36 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function(){
+      if (this.at(1) === undefined) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      this.remove(this.at(0));
+      if (this.at(0)) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function(song) {
+      if (song === this.at(0)) {
+        this.remove(song);
+        if (this.at(0)) {
+          this.playFirst();
+        } else {
+          $('audio').attr('src', '');
+        }
+      }
+      
+      this.remove(song);
+    });
+
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
